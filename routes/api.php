ff2eae9732cb2ec49bc1bfbccd1d8aeb5194fc7d
adrisinaga <fcb.sinaga@gmail.com>
2021-09-07 12:31:11 +0700

    fix: Gantt Chart dynamic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GanttController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/data', '\App\Http\Controllers\GanttController@get');
+
+
+Route::resource('task', '\App\Http\Controllers\TaskController');
+Route::resource('link', '\App\Http\Controllers\LinkController');
